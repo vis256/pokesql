1b57918183548eaf6b3b48f952901a01c2342b16
vis256 <mateusz10077@gmail.com>
2022-12-08 11:18:29 +0100

    Add correct coloring to Region labels

--- Front/src/app/shared/components/region-label/region-label.component.ts
+++ Front/src/app/shared/components/region-label/region-label.component.ts
@@ -1,5 +1,17 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+const colors : any = {
+  'Kanto' : '#7AC74C',
+  'Johto' : '#EE8130',
+  'Hoenn' : '#E2BF65',
+  'Sinnoh' : '#6390F0',
+  'Unova' : '#A8A77A',
+  'Kalos' : '#6F35FC',
+  'Alola' : '#A98FF3',
+  'Galar' : '#96D9D6',
+  'Paldea' : '#705746'
+}
+
 @Component({
   selector: 'app-region-label',
   templateUrl: './region-label.component.html',
@@ -9,8 +21,9 @@ export class RegionLabelComponent implements OnInit {
 
   constructor() { }
 
+  color : string = '#fff';
   ngOnInit(): void {
-
+    this.color = colors[this.region];
   }
 
   @Input()
